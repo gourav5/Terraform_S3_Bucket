4d696e1142d777bcb6aa41d5566439470550f90b
gourav5 <33307086+gourav5@users.noreply.github.com>
2024-10-09 15:04:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
-provider = "AWS" {
-region = "ap-south-1"
+provider "aws" {
+  region = "us-east-1"  # Change to your desired region
 }
+
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "gourav6756tata"
+  bucket = "gourav77889tata"  # Change this to a globally unique name
+  acl    = "private"
 }
+
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.bucket
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.my_bucket.arn
+}
+
