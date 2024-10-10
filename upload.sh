446410cdaf370876db74430b05c6a2aeb34dffac
gourav5 <33307086+gourav5@users.noreply.github.com>
2024-10-10 15:35:51 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -3,11 +3,11 @@
 # Variables
 FILE_PATH="/var/www/web2/index.html"  # Change this to your file path
 BUCKET_NAME="gourav77889tata"   # Change this to your S3 bucket name
-S3_PATH="s3://gourav77889tata"         # Optional: specify the S3 path
+S3_PATH="s3://gourav77889tata/"         # Optional: specify the S3 path
 awsprofile=devops
 
 # Copy file to S3
-aws s3 cp "$FILE_PATH" "$S3_PATH" --recursive --profile devops
+aws s3 cp "$FILE_PATH" "$S3_PATH" --recursive 
 
 # Check if the command was successful
 if [ $? -eq 0 ]; then
