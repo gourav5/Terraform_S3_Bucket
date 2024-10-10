220b7d145723cc85f49a9882dc78b39343967843
gourav5 <33307086+gourav5@users.noreply.github.com>
2024-10-10 14:18:28 +0530

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Variables
-FILE_PATH="/var/www/web2"  # Change this to your file path
+FILE_PATH="/var/www/web2/index.html"  # Change this to your file path
 BUCKET_NAME="gourav77889tata"   # Change this to your S3 bucket name
-S3_PATH="s3://gourav77889tata/"         # Optional: specify the S3 path
+S3_PATH="s3://gourav77889tata"         # Optional: specify the S3 path
 
 # Copy file to S3
 aws s3 cp "$FILE_PATH" "$S3_PATH"
