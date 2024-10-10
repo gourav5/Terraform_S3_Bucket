e29e2ff9ce1ee3f47dd71e70c5d7ac340321ae4e
gourav5 <33307086+gourav5@users.noreply.github.com>
2024-10-10 13:15:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-provider "aws" {
-  region = "ap-south-1"  # Change to your desired region
-}
+#provider "aws" {
+  #region = "ap-south-1"  # Change to your desired region
+#}
 
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "gourav77889tata"  # Change this to a globally unique name
