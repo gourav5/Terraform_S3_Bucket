58afd183a16eccf158a3260d2fa36e4d09218bd6
gourav5 <33307086+gourav5@users.noreply.github.com>
2024-10-10 13:26:09 +0530

    Create upload.sh

---
+++ upload.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Variables
+FILE_PATH="/var/www/web2"  # Change this to your file path
+BUCKET_NAME="gourav77889tata"   # Change this to your S3 bucket name
+S3_PATH="s3://gourav77889tata/"         # Optional: specify the S3 path
+
+# Copy file to S3
+aws s3 cp "$FILE_PATH" "$S3_PATH"
+
+# Check if the command was successful
+if [ $? -eq 0 ]; then
+    echo "File uploaded successfully to $S3_PATH"
+else
+    echo "Error uploading file to S3"
+fi
